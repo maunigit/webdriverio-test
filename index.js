1587028df8e5b4ed4eac283db504fa02b0ddf7ca
Matteo Mirisola <mirimmm94@gmail.com>
2019-10-28 13:14:07 +0100

    add current_package

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const wdio = require("webdriverio");
+const assert = require('assert')
 
 const opts = {
   protocol: 'http',
-  hostname: '192.168.0.253',
+  hostname: '192.168.0.206', //to check
   port: 4723,
   capabilities: {
     deviceName: "MI5",
@@ -18,9 +19,15 @@ const opts = {
 
 async function main () {
   const client = await wdio.remote(opts);
-  const field = await client.$("~reset");
-  const value = await field.getText();
-  assert.equal(value,"reset");
+
+  const current_package = await client.getCurrentPackage();
+  console.log('current_package is: ' + current_package);
+  assert.equal(current_package,"com.carquery.app");
+
+  const button_reset = await client.$("~reset").waitForExist();
+  const reset_text = await button_reset.getText();
+  console.log('reset_text is: ' + reset_text);
+
   await client.deleteSession();
 }
 
