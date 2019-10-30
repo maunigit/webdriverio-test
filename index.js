7f7040c7c35b1b89b262c7489f83a0e2fe847b7f
Matteo Mirisola <mirimmm94@gmail.com>
2019-10-30 11:13:33 +0100

    add caps for virtualbox android machine

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const wdio = require("webdriverio");
-const assert = require('assert')
 
 const opts = {
   protocol: 'http',
@@ -8,8 +7,10 @@ const opts = {
   capabilities: {
     deviceName: "MI5",
     udid: "26060fa3", //macchina android
+    //udid: "192.168.0.206:5555",
     platformName: "Android",
     platformVersion: "8.0.0",
+    //platformVersion: "7.0.0",
     appPackage: "com.carquery.app",
     appActivity: "host.exp.exponent.MainActivity",
     automationName: "UiAutomator2",
@@ -21,30 +22,10 @@ const opts = {
 
 async function main () {
   const client = await wdio.remote(opts);
-
-  //let context = client.getContext();
-
   const res = await client.status();
-  //assert.isObject(res.build);
-
   const current_package = await client.getCurrentPackage();
-  console.log('current_package is: ' + current_package);
-  //assert.equal(current_package,"com.carquery.app");
-
- 
-  //const button_reset = await client.$("~reset");
-  //const reset_text = await button_reset.getText();
-  //console.log('reset_text is: ' + reset_text);
-  
-
+  console.log('current_package is: ' + current_package); 
   const delete_session = await client.deleteSession();
-  //assert.isNull(delete_session);
-
-  /*
-  waitForIsShown (isShown = true) {
-    return $(this.selector).waitForDisplayed(DEFAULT_TIMEOUT, !isShown);
-  }
-  */
 }
 
 main();
